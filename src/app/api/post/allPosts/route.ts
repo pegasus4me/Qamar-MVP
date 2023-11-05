0f0aab39f71e4b0559fa5965e47812eb082f058e
pegasus4me <safoantouil@outlook.fr>
2023-11-05 18:02:04 +0100

    fix error

--- src/app/api/post/allPosts/route.ts
+++ src/app/api/post/allPosts/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/db.server";
 import { NextResponse } from "next/server";
+export const dynamic = 'force-dynamic'
 
 export const GET = async (req: Request) => {
     const findAllPosts = await prisma.post.findMany({
