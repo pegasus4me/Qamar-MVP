d98529e00e46e3c8841c66d59a559e5787574421
pegasus4me <safoantouil@outlook.fr>
2023-11-06 13:45:52 +0100

    session fix #3

--- src/lib/sessionProvider.tsx
+++ src/lib/sessionProvider.tsx
@@ -4,13 +4,6 @@ import { SessionProvider, getSession } from "next-auth/react";
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
-Provider.getInitialProps = async (context : any) => {
-  const { ctx } = context;
-  const session = await getSession(ctx);
 
-  return {
-    session,
-  };
-};
 
 export default Provider;
