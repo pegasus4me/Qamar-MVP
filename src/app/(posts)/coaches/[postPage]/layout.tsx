aa529225c0471b74b1ee7032c31649859baa9883
pegasus4me <safoantouil@outlook.fr>
2023-11-06 14:17:19 +0100

    session fix #6

--- src/app/(posts)/coaches/[postPage]/layout.tsx
+++ src/app/(posts)/coaches/[postPage]/layout.tsx
@@ -1,24 +1,8 @@
 "use client";
 export const dynamic = "force-dynamic";
-import Provider from "@/lib/sessionProvider";
+// import Provider from "@/lib/sessionProvider";
 import { ContextProvider } from "@/lib/contexProvider";
 import { useState } from "react";
-import { Session } from "inspector";
-import { headers } from "next/headers";
-
-async function getSession(cookie: string): Promise<Session> {
-  const response = await fetch(
-    `${process.env.LOCAL_AUTH_URL}/api/auth/session`,
-    {
-      headers: {
-        cookie,
-      },
-    }
-  );
-
-  const session = await response.json();
-  return Object.keys(session).length > 0 ? session : null;
-}
 
 export default async function Layout({
   children,
@@ -27,11 +11,11 @@ export default async function Layout({
 }) {
   const [date, setDate] = useState<string | undefined>();
 
-  const session = await getSession(headers().get("cookie") ?? "");
-
   return (
-    <ContextProvider.Provider value={{ date, setDate }}>
-      {children}
-    </ContextProvider.Provider>
+    <>
+      <ContextProvider.Provider value={{ date, setDate }}>
+        {children}
+      </ContextProvider.Provider>
+    </>
   );
 }
