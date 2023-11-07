6097805d38c8061565709a531f1b05171e2c2fb8
pegasus4me <safoantouil@outlook.fr>
2023-11-07 12:17:57 +0100

    community add

--- src/app/(posts)/coaches/[postPage]/succes/page.tsx
+++ src/app/(posts)/coaches/[postPage]/succes/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import check from "../../../../../../public/assets/check.svg";
@@ -6,17 +6,25 @@ import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 const Succes = () => {
-    const {data:session} = useSession()
+  const { data: session } = useSession();
 
   return (
     <div className="flex justify-center flex-col items-center gap-4 mt-11 p-4">
       <h1 className="text-2xl font-medium opacity-50">
-        Your event have been sheduled 
+        Your event have been sheduled 🎊
       </h1>
       <p>thanks for your trust</p>
+      <p className="text-sm font-medium">
+        do not forget to join the official community{" "}
+        <a href="https://join.slack.com/t/qamarstudio/shared_invite/zt-26lf9rxdw-0N5_RGIkWu1Te2LRdOQxuA" className="text-[#230E49]">
+          join
+        </a>
+      </p>
       <Image src={check} alt="confimation"></Image>
       <Button className="max-w-[300px] bg-[#230E49]">
-        <Link href={`/user/${session?.user.id}/coachings`}>go to your profile</Link>
+        <Link href={`/user/${session?.user.id}/coachings`}>
+          go to your profile
+        </Link>
       </Button>
     </div>
   );
--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,26 +1,58 @@
-import logo from "../../../public/assets/logo.svg"
-import Image from "next/image"
-import { Twitter } from "lucide-react"
-import { FaLinkedin } from "react-icons/fa6"
-import Link from "next/link"
+import logo from "../../../public/assets/logo.svg";
+import Image from "next/image";
+import { Twitter } from "lucide-react";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaSlack } from "react-icons/fa6";
+import Link from "next/link";
 const Footer = () => {
-
-    return <footer className="bg-neutral-100 mt-7 max-w-[70%] m-auto mb-5 rounded-md p-3  flex-col border">
-        <div className="flex justify-around items-center">
-            <div>
-                <Image src={logo} alt="logo"></Image>
-            </div>
-            <div className=" font-medium text-neutral-500 mt-3 flex  flex-col gap-1 p-2">
-                <a href="mailto:contact@qamarstudio.com" className="text-[#230E49]">contact us</a>
-                <Link href="/coaches" className="flex hover:text-[#230E49] transition-all">browse coaches</Link>
-                <Link href="/coaches" className="flex hover:text-[#230E49] transition-all">login</Link>
-                <Link href="/coaches" className="flex hover:text-[#230E49] transition-all">register</Link>
-            </div>
+  return (
+    <footer className="bg-neutral-100 mt-7 max-w-[70%] m-auto mb-5 rounded-md p-3  flex-col border">
+      <div className="flex justify-around items-center">
+        <div>
+          <Image src={logo} alt="logo"></Image>
         </div>
-        <div className="flex gap-2 text-end mt-2 p-2">
-            <a href="https://www.linkedin.com/company/qamar-studio/about/"><FaLinkedin className="text-xl"/></a>
-            <a href="https://twitter.com/QamarStudio_"><Twitter/></a>
+        <div className=" font-medium text-neutral-500 mt-3 flex  flex-col gap-1 p-2">
+          <a href="mailto:contact@qamarstudio.com" className="text-[#230E49]">
+            contact us
+          </a>
+          <Link
+            href="/coaches"
+            className="flex hover:text-[#230E49] transition-all"
+          >
+            browse coaches
+          </Link>
+          <Link
+            href="/coaches"
+            className="flex hover:text-[#230E49] transition-all"
+          >
+            login
+          </Link>
+          <Link
+            href="/coaches"
+            className="flex hover:text-[#230E49] transition-all"
+          >
+            register
+          </Link>
+          <a
+            href="https://join.slack.com/t/qamarstudio/shared_invite/zt-26lf9rxdw-0N5_RGIkWu1Te2LRdOQxuA"
+            className="flex hover:text-[#230E49] transition-all"
+          >
+            community
+          </a>
         </div>
+      </div>
+      <div className="flex gap-2 text-end mt-2 p-2 items-center">
+        <a href="https://www.linkedin.com/company/qamar-studio/about/">
+          <FaLinkedin className="text-xl" />
+        </a>
+        <a href="https://twitter.com/QamarStudio_">
+          <Twitter />
+        </a>
+        <a href="https://twitter.com/QamarStudio_">
+          <FaSlack className="text-xl" />
+        </a>
+      </div>
     </footer>
-}
-export default Footer
+  );
+};
+export default Footer;
